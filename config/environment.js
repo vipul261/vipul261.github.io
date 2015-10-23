910cfb2ed66f711e4de1c787a9c4f86cc15342f3
vipul261 <vipul261@gmail.com>
2015-10-23 17:29:51 +0530

    test

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'my-portfolio',
     environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
+    baseURL: '/my-portfolio',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
