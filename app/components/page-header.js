c10e87c914c75f50195ce0e4912ea023c7091b77
vipul261 <vipul261@gmail.com>
2015-10-26 13:48:05 +0530

    Adds owner phone and social profiles in header

--- app/components/page-header.js
+++ app/components/page-header.js
@@ -1,5 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNames: ['page-header']
+    classNames: ['page-header'],
+    showSocailBar: false,
+    socialAccounts: [
+        {
+            name: "Facebook",
+            icon: "facebook",
+            url: "https://www.facebook.com/vipulgarg261"
+        },
+        {
+            name: "Twitter",
+            icon: "twitter",
+            url: "https://twitter.com/vipul_261"
+        },
+        {
+            name: "Git Hub",
+            icon: "github",
+            url: "https://github.com/vipul261"
+        }
+    ],
+    actions:{
+        toggleSocial(){
+            this.set('showSocailBar', (event.type !== 'mouseout'));
+        }
+    }
 });
