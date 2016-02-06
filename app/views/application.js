7a3c9877e59da11678bd5ba693e3c2706091af9f
Vipul Garg <vipul261@gmail.com>
2016-02-06 11:54:37 +0530

    Upgraded to Ember 2.3.0

--- app/views/application.js
+++
@@ -1,6 +0,0 @@
-import Ember from "ember";
-
-export default Ember.View.extend({
-  classNames: ['page-full-height']
-  // this is the application view
-});
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'my-portfolio',
     environment: environment,
     baseURL: '/',
-    locationType: 'hash',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
